52b5b3757278bfc3a0098c8be03b90b8656345e0
TJ James <jamestw13@gmail.com>
2021-09-09 18:51:16 -0500

    added prompts

--- Develop/script.js
+++ Develop/script.js
@@ -1,16 +1,27 @@
 // Assignment code here
 
+function generatePassword() {
+	var password = "lorem";
+	var length = window.prompt(
+		"How long would you like your password to be?\nPlease enter a number between 8 and 128."
+	);
+	var includeLower = window.confirm("Include LOWERCASE characters?");
+	var includeUpper = window.confirm("Include UPPERCASE characters?");
+	var includeNumber = window.confirm("Include NUMERICAL characters?");
+	var includeSpecial = window.confirm("Include SPECIAL characters?");
+
+	return password;
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+	var password = generatePassword();
+	var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
-
+	passwordText.value = password;
 }
 
 // Add event listener to generate button
