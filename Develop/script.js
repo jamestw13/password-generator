df7f94f2859ee4ce03b89357df8bf74d6bc64591
TJ James <jamestw13@gmail.com>
2021-09-10 01:02:42 -0500

    streamlined code style

--- Develop/script.js
+++ Develop/script.js
@@ -1,150 +1,87 @@
-// Assignment code here
+// Constants
+const MIN_PWORD_LENGTH = 8;
+const MAX_PWORD_LENGTH = 128;
+const CHAR_ARRAYS = {
+	lower: ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"],
+	upper: ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"],
+	number: ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
+	special: ["!","\"","#","$","%","&","'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","]","^",")","`","{","|","}","~"],
+};
+
+// CHARACTER PICKER FUNCTION - returns a random character from a given array of characters
+function characterPicker(array) {
+	// randomly select an index from the array
+	var randomIndex = Math.floor(Math.random() * array.length);
+	// return the character at that index from the array
+	return array[randomIndex];
+}
+
+// MAIN GENERATION FUNCTION
 function generatePassword() {
 	var password = "";
 	var passwordLength = "";
+
+	// Prompt for password length
 	while (!passwordLength) {
+
+		// Get user input for length
 		passwordLength = window.prompt(
-			"How long would you like your password to be?\nPlease enter a number between 8 and 128."
-		);
+			"How long would you like your password to be?\nPlease enter a number between " +	MIN_PWORD_LENGTH + " and " +	MAX_PWORD_LENGTH + ".");
+
+		// Parse string to int
 		passwordLength = parseInt(passwordLength);
-		if (passwordLength < 8 || passwordLength > 128) {
+
+		// Validate password length
+		if (passwordLength < MIN_PWORD_LENGTH || passwordLength > MAX_PWORD_LENGTH) {
 			passwordLength = "";
 		}
 	}
 
-	console.log(passwordLength);
+	// Prompts for password criteria
+	var includeLower = window.confirm("Click OK to include lowercase characters in your password.\nClick Cancel to exclude them.\nExample: " +	CHAR_ARRAYS.lower.join(""));
+	var includeUpper = window.confirm("Click OK to include uppercase characters in your password.\nClick Cancel to exclude them.\nExample: " +	CHAR_ARRAYS.upper.join(""));
+	var includeNumber = window.confirm("Click OK to include numeric characters in your password.\nClick Cancel to exclude them.\nExample: " +	CHAR_ARRAYS.number.join(""));
+	var includeSpecial = window.confirm("Click OK to include special characters in your password.\nClick Cancel to exclude them.\nExample: " +	CHAR_ARRAYS.special.join(""));
 
-	var includeLower = window.confirm("Include LOWERCASE characters?");
-	var includeUpper = window.confirm("Include UPPERCASE characters?");
-	var includeNumber = window.confirm("Include NUMERICAL characters?");
-	var includeSpecial = window.confirm("Include SPECIAL characters?");
-
+	// Distribute characters in password based on criteria
 	while (passwordLength > 0) {
+
+		// Add lowercase characters
 		if (includeLower) {
-			var lowerArray = [
-				"a",
-				"b",
-				"c",
-				"d",
-				"e",
-				"f",
-				"g",
-				"h",
-				"i",
-				"j",
-				"k",
-				"l",
-				"m",
-				"n",
-				"o",
-				"p",
-				"q",
-				"r",
-				"s",
-				"t",
-				"u",
-				"v",
-				"w",
-				"x",
-				"y",
-				"z",
-			];
-
-			password += characterPicker(lowerArray);
+			password += characterPicker(CHAR_ARRAYS.lower);
 			passwordLength -= 1;
-
 			if (passwordLength <= 0) {
 				break;
 			}
 		}
+
+		// Add uppercase characters
 		if (includeUpper) {
-			var upperArray = [
-				"A",
-				"B",
-				"C",
-				"D",
-				"E",
-				"F",
-				"G",
-				"H",
-				"I",
-				"J",
-				"K",
-				"L",
-				"M",
-				"N",
-				"O",
-				"P",
-				"Q",
-				"R",
-				"S",
-				"T",
-				"U",
-				"V",
-				"W",
-				"X",
-				"Z",
-				"Y",
-			];
-			password += characterPicker(upperArray);
+			password += characterPicker(CHAR_ARRAYS.upper);
 			passwordLength -= 1;
-
 			if (passwordLength <= 0) {
 				break;
 			}
 		}
+
+		// Add numerical characters
 		if (includeNumber) {
-			var numberArray = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
-			password += characterPicker(numberArray);
+			password += characterPicker(CHAR_ARRAYS.number);
 			passwordLength -= 1;
-
 			if (passwordLength <= 0) {
 				break;
 			}
 		}
-		if (includeSpecial) {
-			var specialArray = [
-				"!",
-				'"',
-				"#",
-				"$",
-				"%",
-				"&",
-				"'",
-				"(",
-				")",
-				"*",
-				"+",
-				",",
-				"-",
-				".",
-				"/",
-				":",
-				";",
-				"<",
-				"=",
-				">",
-				"?",
-				"@",
-				"[",
-				"]",
-				"^",
-				")",
-				"`",
-				"{",
-				"|",
-				"}",
-				"~",
-			];
-			password += characterPicker(specialArray);
-			passwordLength -= 1;
 
+		// Special Characters
+		if (includeSpecial) {
+			password += characterPicker(CHAR_ARRAYS.special);
+			passwordLength -= 1;
 			if (passwordLength <= 0) {
 				break;
 			}
 		}
 	}
-
 	return password;
 }
 
@@ -161,10 +98,3 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-function characterPicker(array) {
-	// randomly select an index from the array
-	var randomIndex = Math.floor(Math.random() * array.length);
-	// return the character at that index from the array
-	return array[randomIndex];
-}
